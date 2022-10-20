7c9816ee492137b2b2ec7698a0bc01218d323fd8
AirineiAndrei <acairinei17@gmail.com>
2022-10-20 19:19:18 +0300

    List and Dictionaries practice

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+List<int> someNumbers = new List<int>();
+
+someNumbers.Add(1);
+someNumbers.Add(2);
+someNumbers.Add(3); 
+someNumbers.Add(4); 
+someNumbers.Add(5);
+
+foreach (int number in someNumbers)
+{
+    Console.Write(number);
+    Console.Write(' ');
+}
+Console.WriteLine();
+
+List<String> words = new List<String>();
+
+words.Add("Ana");
+words.Add("are");
+words.Add("mere");
+words.Add("multe!");
+for(int i = 0; i < words.Count; i++)
+    Console.WriteLine(words[i]);
+
+var wordDict = new Dictionary<int, string>();
+
+for (int i = 0; i < words.Count; i++)
+    wordDict[i] = words[i];
+
+foreach (var it in wordDict)
+{
+    Console.WriteLine($"{it.Key} with val {it.Value}");
+}
