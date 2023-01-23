c221f30216ca879177cb27c583a9aa1934d3002e
AirineiAndrei <acairinei17@gmail.com>
2023-01-23 17:19:07 +0200

    Initial structure - Added Data layer

---
+++ Proiect DAW/DAL/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proiect_DAW.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DAW;");
+        }
+    }
+}
